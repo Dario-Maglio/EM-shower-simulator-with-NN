b2654dd03daa42e897eb3c72fd4df31ba4caef0a
dpassaro <d.passaro1@studenti.unipi.it>
2022-02-09 11:22:01 +0100

    Restore train in class_Gaan

--- EM_shower_simulator/make_models.py
+++ EM_shower_simulator/make_models.py
@@ -118,7 +118,7 @@ def make_generator_model():
 
     output = (Conv3DTranspose(1, KERNEL, use_bias=False,
                               activation="tanh", name="Fake_image")(gen))
-    output = Lambda(zero_suppression, name="Fake_image_zero_suppression")(output)
+    #output = Lambda(zero_suppression, name="Fake_image_zero_suppression")(output)
     logger.info(f"Shape of the generator output: {output.get_shape()}")
     assert output.get_shape().as_list()==[None, *GEOMETRY], error
 

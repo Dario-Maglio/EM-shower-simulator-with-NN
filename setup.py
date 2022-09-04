382d1ace4642c6fe589adb0503a35f28d4fe814c
Dario Cafasso Maglio <71833726+Dario-Maglio@users.noreply.github.com>
2022-09-04 12:14:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
    Installing from local source in Development Mode, i.e. in such a way that the
    project appears to be installed, but yet is still editable from the src tree:
 
-      $ git clone https://github.com/Dario-Caf/EM-shower-simulator-with-NN.git
+      $ git clone https://github.com/Dario-Maglio/EM-shower-simulator-with-NN.git
       $ cd EM-shower-simulator-with-NN
       $ python3 -m pip install -e .
 
